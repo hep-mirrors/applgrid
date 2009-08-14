a42a1c9b39989d7a3eab2b1caf47f31ff24ac1da
sutt <sutt@32d2155c-4299-4188-a341-1b4f4da92f88>
2009-08-14 16:23:58 +0000

    add tancredi ubunto setup script
    git-svn-id: https://svn.hepforge.org/applgridsvn/trunk@34 32d2155c-4299-4188-a341-1b4f4da92f88

--- setup_tancredi.sh
+++ setup_tancredi.sh
@@ -10,6 +10,8 @@ export LHAPDF=/home/tcarli/lhapdf-5.7.0
 export PATH=/usr/sue/bin:/usr/local/bin:/usr/local/bin/X11:/usr/bin:/bin:/usr/bin/X11
 export LD_LIBRARY_PATH=/usr/lib
 
+# this adds lhapdf/bin to path so that the essential  
+# lhapdf-config script can be found
 export PATH=$ROOTSYS/bin:$LHAPDF/bin:$PATH
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 

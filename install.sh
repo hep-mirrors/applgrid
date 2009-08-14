8779b01872e3b6beee5cc54160f3282158865c30
sutt <sutt@32d2155c-4299-4188-a341-1b4f4da92f88>
2009-08-14 18:34:54 +0000

    add sh functions to install.sh
    git-svn-id: https://svn.hepforge.org/applgridsvn/trunk@36 32d2155c-4299-4188-a341-1b4f4da92f88

--- install.sh
+++ install.sh
@@ -5,13 +5,6 @@
 #  setup everything
 ###############################################
 
-if [ "$COPYDIR" = "" ]; then 
-  export COPYDIR=$PWD
-fi
-
-echo "copy directory " $COPYDIR
-
-
 if [ "$BASEDIR" = "" ]; then 
   export BASEDIR=$PWD
 fi
@@ -19,9 +12,10 @@ export INSTALLBASE=$BASEDIR
 
 echo "base directory " $BASEDIR 
 if [ -e $BASEDIR ]; then 
-   echo "$BASEDIR already exists"
+   echo "installing in $BASEDIR"
 else
-   mkdir $BASEDIR
+   echo "$BASEDIR does not exists" 
+   exit
 fi
 
 cd $BASEDIR
@@ -40,7 +34,6 @@ if [ "$LHAPDFFOUND" = "" ]; then
   echo "lhapdf not setup"
   exit
 fi
-
 
 
 
@@ -72,205 +65,204 @@ export DYLD_LIBRARY_PATH=$BASEDIR/lib:$BASEDIR/libexec:$LHAPDFLIB:$DYLD_LIBRARY_
 
 ############################
 # INSTALL THE APPL GRID CODE 
-# AND PDF EVOLUTION CODE
 ############################
-cd $BASEDIR/appl_grid/src
+install_appl_grid() { 
 
-if [ "$1" = "clean" ]; then
- make clean
-fi
+    cd $BASEDIR/appl_grid/src
+
+    if [ "$1" = "clean" ]; then
+	make clean
+    fi
+
+    make install
+
+}
 
 
-make install
+
+############################
+# INSTALL EVOLUTION CODE
+############################
+install_pdf() { 
+
+    cd $BASEDIR/pdf-conv-1.0/src
+    
+    if [ "$1" = "clean" ]; then
+	make clean
+    fi
+    
+    make  install
+    
+}
 
 
-cd $BASEDIR/pdf-conv-1.0/src
-
-if [ "$1" = "clean" ]; then
- make clean
-fi
-
-make  install
 
 ######################
 # INSTALL MCFM
 ######################
-
-cd $BASEDIR
-
-
-############################################
-# BUILD MCFM TEST EXECUTABLE
-############################################
-cd $BASEDIR/mcfm
-make install
+install_mcfm() { 
+    cd $BASEDIR
+    cd $BASEDIR/mcfm
+    make install    
+}
 
 
 ############################################
 # RUN THE MCFM TEST EXECUTABLE
 ############################################
+run_mcfm() { 
 
-cd $BASEDIR/mcfm/run
+    cd $BASEDIR/mcfm/run
 
-../exe/*/mcfm Winput.DAT >&  mcfm-0.log
-../exe/*/mcfm Winput.DAT >&  mcfm-1.log
-../exe/*/stand grid-30-Wweight_eta4.root
+    ../exe/*/mcfm Winput.DAT >&  mcfm-0.log
+    ../exe/*/mcfm Winput.DAT >&  mcfm-1.log
+    ../exe/*/stand grid-30-Wweight_eta4.root
+    
+}
 
-# echo EXITING......
-# exit
 
-
+install_nlojet() { 
 #############################
 # INSTALL NLOJET PDF LIBRARY 
 #############################
-
-cd $BASEDIR
-
-echo  "bld-pdf" 
-
-if [ "$1" = "clean" ]; then
-    rm -rf bld-pdf
-fi
-
-if [ "$1" = "nloclean" ]; then
-    rm -rf bld-pdf
-fi
-
-
-if [ -e bld-pdf ]; then 
-  echo "bld-pdf already exists" 
-else
-
-  mkdir bld-pdf
-
-  if [ "$1" = "clean" ]; then
-    make clean
-  fi
-
-  cd bld-pdf
-  ../lhpdf-1.0.0/configure --prefix=$BASEDIR
-  # > $BASEDIR/lhpdf.log
-
-  make        
-  #  >> $BASEDIR/lhpdf.log
-
-  make install 
-  #  >> $BASEDIR/lhpdf.log
-
-fi
+    
+    cd $BASEDIR
+    
+    echo  "bld-pdf" 
+    
+    if [ "$1" = "clean" ]; then
+	rm -rf bld-pdf
+    fi
+    
+    if [ "$1" = "nloclean" ]; then
+	rm -rf bld-pdf
+    fi
+    
+    
+    if [ -e bld-pdf ]; then 
+	echo "bld-pdf already exists" 
+    else
+	
+	mkdir bld-pdf
+	
+	if [ "$1" = "clean" ]; then
+	    make clean
+	fi
+	
+	cd bld-pdf
+	../lhpdf-1.0.0/configure --prefix=$BASEDIR
+	
+	make        
+	make install 
+	
+    fi
 
 
 ######################
 # INSTALL NLOJET
 ######################
+    
+    cd $BASEDIR
+    
+    
+    echo "bld-nlo" 
+    if [ -e bld-nlo ]; then 
+	echo "bld-nlo already exists" 
+    else
+	mkdir bld-nlo
+	
+	cd   bld-nlo
+	
+	echo "nlojet"
+	../nlojet++-4.0.1/configure --prefix=$BASEDIR 
+	
+	if [ "$1" = "clean" ]; then
+	    make clean
+	fi
+	
+	make          
+	make install 
+	
+    fi
+    
+    echo $PATH
+    echo "which create-nlojet-user"
 
-cd $BASEDIR
-
-
-echo "bld-nlo" 
-if [ -e bld-nlo ]; then 
-  echo "bld-nlo already exists" 
-else
-  mkdir bld-nlo
-
-  cd   bld-nlo
-
-  echo "nlojet"
-  ../nlojet++-4.0.1/configure --prefix=$BASEDIR 
-  # cd blo> $BASEDIR/nlo.log
-
-  if [ "$1" = "clean" ]; then
-    make clean
-  fi
-
-  make          
-  # >> $BASEDIR/nlo.log
-  make install 
-  # >> $BASEDIR/nlo.log
-
-fi
-
-echo $PATH
-echo "which create-nlojet-user"
-#echo "why. oh why, do we need these 'helper' routines that make automating things so difficult..."
-which create-nlojet-user
-
+#   which create-nlojet-user
+    
+}
 
 
-######################
-# THREEJET MODULES
-######################
-
-# cd $BASEDIR
-
-# echo "modules"
-# if [ -e modules-4.0.0.tar.gz  ]; then 
-#   echo "modules-4.0.0 already exists"
-# else
-#   cp $COPYDIR/modules-4.0.0.tar.gz modules-4.0.0.tar.gz
-# fi
-
-# tar -zxf modules-4.0.0.tar.gz
-
-# cd "modules-4.0.0/pp->jets/hep-ph:0307268/ThreeJet"
-# make all  
-
-
-#  nlojet++ --calculate -u kT-scale.la
-#  nlojet++ --calculate -u kT-xsec.la
-#  nlojet++ --calculate -u cone-scale.la
 
 
 ######################
 # INSTALL GRID FILLING
 # MODULES 
 ######################
+install_nlojet_module() { 
 
+    echo "nlojet-module"
+    if [ -e $BASEDIR/nlojet-module  ]; then
+	echo "cd nlojet-module"
+	cd $BASEDIR/nlojet-module
+    else 
+	echo "$BASEDIR/nlojet-module doesn't exist"
+	exit
+    fi
+    
 
-cd $BASEDIR
-
-echo "nlojet-module"
-if [ -e $BASEDIR/nlojet-module  ]; then
-   echo "nlojet-module already exists"
-else 
-#   cp $COPYDIR/ap.tgz ap.tgz
-    echo doing nowt
-fi
-
-	
-echo "building fillgrid..."
-cd nlojet-module
-
-if [ "$1" = "clean" ]; then
- make clean
-fi
-
-make install
-
-echo PATH is: $PATH
-
-echo LD_LIBRARY_PATH: $LD_LIBRARY_PATH
-
-if [ -e PDFsets ]; then 
-  ls -ld PDFsets
-else
-  ln -s `lhapdf-config --pdfsets-path` .
-  ls -ld PDFsets
-fi
-
-
-
-which nlojet++
-# nlojet++ --calculate -u fillgrid.la --max-event 20000
-nlojet++ --calculate -u fillgrid.la 
-nlojet++ --calculate -u fillgrid.la 
-
-if [ -e output/weight_c.root ]; then 
-  ./standalone output/weight_c.root
-  ./stand      output/weight_c.root
-else
-  echo "nlojet grid code did not complete correctly" 
-fi
+    echo "building fillgrid..."
+    
+    if [ "$1" = "clean" ]; then
+	make clean
+    fi
+    
+    make install
+    
+    echo PATH is: $PATH
+    echo LD_LIBRARY_PATH: $LD_LIBRARY_PATH
+    
+    if [ -e PDFsets ]; then 
+	ls -ld PDFsets
+    else
+	ln -s `lhapdf-config --pdfsets-path` .
+	ls -ld PDFsets
+    fi
+    
+}
 
 
 
+run_nlojet_module () { 
+
+    cd $BASEDIR/nlojet-module
+
+    if [ -e output/weight_c.root ]; then
+	echo "nlojet already executed"
+    else  
+	which nlojet++
+#       nlojet++ --calculate -u fillgrid.la --max-event 20000
+	nlojet++ --calculate -u fillgrid.la 
+	nlojet++ --calculate -u fillgrid.la 
+    fi
+
+    if [ -e output/weight_c.root ]; then 
+	./standalone output/weight_c.root
+	./stand      output/weight_c.root
+    else
+	echo "nlojet grid code did not complete correctly" 
+    fi
+    
+}
+
+
+
+ARGS=$*
+
+install_appl_grid   $ARGS
+install_pdf         $ARGS
+install_mcfm        $ARGS
+run_mcfm        
+install_nlojet         $ARGS
+install_nlojet_module  $ARGS
+run_nlojet_module 

6b667f4076b2f39134ab0b33df63bfb471d15059
sutt <sutt@32d2155c-4299-4188-a341-1b4f4da92f88>
2009-09-27 19:21:06 +0000

    add internal hoppet check and initialisation routines
    git-svn-id: https://svn.hepforge.org/applgridsvn/trunk@92 32d2155c-4299-4188-a341-1b4f4da92f88

---
+++ appl_grid/appl_grid/hoppet_init.h
@@ -0,0 +1,56 @@
+// emacs: this is -*- c++ -*-
+//
+//   @file    hoppet_init.h        
+//
+//                   
+// 
+//   Copyright (C) 2007 M.Sutton (sutt@cern.ch)    
+//
+//   $Id: hoppet_init.h, v0.0   Sat 26 Sep 2009 13:24:26 BST sutt $
+
+
+#ifndef __HOPPETINIT_H
+#define __HOPPETINIT_H
+
+#include <iostream>
+#include <vector>
+#ifdef HOPPET
+#include "hoppet_v1.h"
+#endif
+
+class hoppet_init {
+
+public:
+
+  hoppet_init();
+
+  virtual ~hoppet_init();
+
+  void hoppet_init::fillCache( void (*pdf)(const double&, const double&, double* )  );
+  
+  bool hoppet_init::compareCache( void (*pdf)(const double&, const double&, double* )  );
+
+private:
+
+  std::vector<double> m_cache;
+
+};
+
+
+
+inline std::ostream& operator<<( std::ostream& s, const hoppet_init& _h ) { 
+  return s;
+}
+
+
+#endif  // __HOPPETINIT_H 
+
+
+
+
+
+
+
+
+
+

d45ee4de5622ed4648d941c0db6bd635c6e8d9e6
sutt <sutt@32d2155c-4299-4188-a341-1b4f4da92f88>
2009-10-09 13:13:43 +0000

    fix makefile and hoppet.sh for compilation without hoppet
    git-svn-id: https://svn.hepforge.org/applgridsvn/trunk@97 32d2155c-4299-4188-a341-1b4f4da92f88

--- bin/hoppet.sh
+++ bin/hoppet.sh
@@ -3,6 +3,19 @@
 # echo "hoppet.sh $1"
 # ls -l $1
 
+if [ "$1" == "" ]; then 
+  exit -1
+fi
+
+if [ "$1" == "--lib" ]; then 
+  if [ "$4" == "" ]; then 
+      exit -1
+  fi
+  if [ -e $2 ]; then 
+      echo $3 $4  
+  fi
+fi
+
 if [ -e $1 ]; then 
   echo "-DHOPPET"
 fi

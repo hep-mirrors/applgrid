7d6439fcd95b3995a7f508c2143acc20de967a00
tcarli <tcarli@32d2155c-4299-4188-a341-1b4f4da92f88>
2009-10-09 13:53:33 +0000

    some suggestion ofr setup at SLC4
    git-svn-id: https://svn.hepforge.org/applgridsvn/trunk@98 32d2155c-4299-4188-a341-1b4f4da92f88

--- setup_tancredi.sh
+++ setup_tancredi.sh
@@ -7,12 +7,26 @@ fi
 
 
 # How to access the svn directory
-#export SVNUSR=svn+ssh://svn.cern.ch/reps/atlasusr/tcarli
-
+#svn co svn+ssh://login.hepforge.org/hepforge/svn/applgrid/
+#
+#
+# Ubunuty 9 set-up on my machine
 #export ROOTSYS=/home/tcarli/root/root5.24
-export LHAPDF=/home/tcarli/lhapdf-5.7.0
-
-
+#export LHAPDF=/home/tcarli/lhapdf-5.7.0
+#
+#
+# SLC4 set-up, 64-bit machine
+export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.24.00/slc4_amd64_gcc34/root
+export LHAPDF=/afs/cern.ch/sw/lcg/external/MCGenerators/lhapdf/5.7.1/slc4_amd64_gcc34
+#
+#
+# SLC5 set-up 43-bit machine
+#export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.24.00/x86_64-slc5-gcc43-opt/root
+#export LHAPDF=/afs/cern.ch/sw/lcg/external/MCGenerators/lhapdf/5.7.1/x86_64-slc5-gcc43-opt
+#export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.24.00/i686-slc5-gcc43-opt/root
+#export LHAPDF=/afs/cern.ch/sw/lcg/external/MCGenerators/lhapdf/5.7.1/i686-slc5-gcc43-opt
+#
+#
 export PATH=/usr/sue/bin:/usr/local/bin:/usr/local/bin/X11:/usr/bin:/bin:/usr/bin/X11
 export LD_LIBRARY_PATH=/usr/lib
 

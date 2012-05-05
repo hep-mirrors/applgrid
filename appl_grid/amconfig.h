66205b1c280245e75fdd9d1930c610492d1903da
sutt <sutt@32d2155c-4299-4188-a341-1b4f4da92f88>
2012-05-05 18:21:05 +0000

    update applgrid version number
    git-svn-id: https://svn.hepforge.org/applgridsvn/trunk@296 32d2155c-4299-4188-a341-1b4f4da92f88

--- appl_grid/amconfig.h
+++ appl_grid/amconfig.h
@@ -57,19 +57,16 @@
 #define PACKAGE_NAME "APPLgrid"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "APPLgrid 1.2.4"
+#define PACKAGE_STRING "APPLgrid 1.2.6"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "applgrid"
 
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.4"
+#define PACKAGE_VERSION "1.2.6"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.2.4"
+#define VERSION "1.2.6"
